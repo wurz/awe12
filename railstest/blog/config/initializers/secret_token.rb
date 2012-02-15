3ee8568ccd062f54ade04849c4613eb08385c765
Jan <hoernle@tzi.de>
2012-02-15 14:03:39 +0100

    First rails commit, including users and microposts

---
+++ railstest/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '65e04aea6331b18057fb2cea174b57381597e89175617267456ff5a46bbf6ed40f64e4405bb83b25131265a814ed734ed5dc926556a36838eecb163ae585228b'
